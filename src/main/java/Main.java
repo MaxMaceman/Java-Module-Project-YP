9385b16f422ee0541f41ecf582523087f94abb5f
MaxMaceman <123957354+MaxMaceman@users.noreply.github.com>
2023-09-07 02:12:59 +0300

    Revert "Д.з. 05.09.2023"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,6 @@
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int persons;
-
-        while (true) {
-            System.out.println("На скольких человек необходимо разделить счет?");
-            if (scanner.hasNextInt()) {
-                persons = scanner.nextInt();
-                if (persons > 1)
-                    break;
-                else {
-                    System.out.println("Ошибка:\n" + "Введенное значение должно быть больше 1-го!");
-                }
-            } else {
-                System.out.println("Ошибка:\n" + "Введенное значение должно быть целым числом!");
-                scanner.next();
-            }
-        }
-
-        Calculator calculator = new Calculator(persons);
-        calculator.start();
+        System.out.println("Hello world!");
     }
 }
